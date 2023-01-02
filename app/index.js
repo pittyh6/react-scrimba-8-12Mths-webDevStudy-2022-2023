fffa05d1ba4a5a7797932af51ef3e06be6ec9428
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-01-02 20:41:40 +1100

    config files, render h1, ul, li

---
+++ app/index.js
@@ -0,0 +1,12 @@
+ReactDOM.render(<h1>Hiiii, darling</h1>, document.getElementById("root"))
+
+ReactDOM.render(<h1>By memory</h1>, document.getElementById("memory"))
+
+/*Render an ul with 2+ li's*/
+ReactDOM.render(
+    <ul>
+        <li>List 1</li>
+        <li>List 2</li>
+    </ul>,
+    document.getElementById('list')
+)
