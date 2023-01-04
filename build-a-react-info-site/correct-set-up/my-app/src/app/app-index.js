2afd6276dea2af7cd960e2cbba949dec1bc8016c
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-01-04 12:22:47 +1100

    config react

---
+++ build-a-react-info-site/correct-set-up/my-app/src/app/app-index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const navBar = (
+    <nav>
+        <h1>Pittyh6</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+ReactDOM.render(navBar, document.getElementById("root"))
