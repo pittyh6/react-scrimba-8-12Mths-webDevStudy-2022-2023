61ffc29a6f3a2f13f1df67508c85699c2310892e
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-01-05 11:04:51 +1100

    react 17 -> 18

--- build-a-react-info-site/correct-set-up/my-set/src/App.js
+++ build-a-react-info-site/correct-set-up/my-set/src/App.js
@@ -1,30 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import React from "react"
-import ReactDOM from "react-dom"
-
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 function App() {
-  
+  //lesson 10
   const navbar = (
-      <nav>
-          <h1>Bob's Bistro</h1>
-          <ul>
-              <li>Menu</li>
-              <li>About</li>
-              <li>Contact</li>
-          </ul>
-      </nav>
-  )
-  
-  ReactDOM.render(navbar, document.getElementById("root"))
-
-  return (
-    <div className="App">
-      
-      <h1>oi</h1>
-    </div>
+    <nav>
+      <h1>Pitty</h1>
+      <ul>
+        <li>Menu</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
   );
+  //ReactDOM.render(navbar, document.getElementById("root"));
+
+  //leson 11
+  // change the way you render from (ReactDOM.render(navbar, document.getElementById("root"));)
+  // to(ReactDOM.createRoot(document.getElementById("root")).render(navbar);)
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(navbar);
 }
 
 export default App;
