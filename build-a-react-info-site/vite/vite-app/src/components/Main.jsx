56e643ce4cd91fe51003991d097644db95345294
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-01-08 11:53:20 +1100

    nav and main section style and jsx

--- build-a-react-info-site/vite/vite-app/src/components/Main.jsx
+++ build-a-react-info-site/vite/vite-app/src/components/Main.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-function Main(){
-    return(
-        <h1>Main goes here</h1>
-    )
+function Main() {
+  return (
+    <main className="main-content">
+      <h1 className="main-content-title">Fun facts about React</h1>
+      <ul className="items">
+        <li className="item">Was first released in 2013</li>
+        <li className="item">Was originally created by Jordan Walke</li>
+        <li className="item">Has well over 100k stars on GitHub</li>
+        <li className="item">Is maintained by Facebook</li>
+        <li className="item">Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
 }
 export default Main;
--- build-a-react-info-site/vite/vite-app/src/components/NavBar.jsx
+++ build-a-react-info-site/vite/vite-app/src/components/NavBar.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import logo from "/src/assets/react.svg";
 
-function NavBar(){
-    return(
-        <h1>Navbar goes here</h1>
-    )
+function NavBar() {
+  return (
+    <div className="navbar">
+      <div className="brand">
+        <img src={logo} alt="image logo react" className="nav-logo"/>
+        <h3 className="nav-name-brand">ReactFacts</h3>
+      </div>
+        <h4 className="nav-title">React Course - Project 1</h4>
+    </div>
+  );
 }
 
 export default NavBar;
