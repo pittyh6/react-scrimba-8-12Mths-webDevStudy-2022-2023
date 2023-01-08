94ed8298e23d84ece644ae48796cb6cfd3b16d13
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-01-08 20:31:49 +1100

    navbar

---
+++ airBnb-clone/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from './assets/logo.png';
+
+export default function App() {
+  return (
+    <nav className="nav">
+      <img src={logo} alt="AirBnb logo Image" className="nav-logo"/>
+    </nav>
+  )
+}
+
