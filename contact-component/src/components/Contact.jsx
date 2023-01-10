33c703c9a5972502f9c229c78e6b7d639cafa246
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-01-11 10:00:26 +1100

    destructuring props

--- contact-component/src/components/Contact.jsx
+++ contact-component/src/components/Contact.jsx
@@ -1,20 +1,39 @@
 import React from "react";
 import App from "../App";
 
-export default function Contact(props) {
-    console.log(props)
+export default function Contact({img, name, phone, email}) {
+
   return (
     <div className="contact-card">
-      <img src={props.img} />
-      <h3>{props.name}</h3>
+      <img src={img} />
+      <h3>{name}</h3>
       <div className="info-group">
         <img src="./images/phone-icon.png" />
-        <p>{props.phone}</p>
+        <p>{phone}</p>
       </div>
       <div className="info-group">
         <img src="./images/mail-icon.png" />
-        <p>{props.email}</p>
+        <p>{email}</p>
       </div>
     </div>
   );
 }
+
+
+// export default function Contact(props) {
+//     console.log(props)
+//   return (
+//     <div className="contact-card">
+//       <img src={props.img} />
+//       <h3>{props.name}</h3>
+//       <div className="info-group">
+//         <img src="./images/phone-icon.png" />
+//         <p>{props.phone}</p>
+//       </div>
+//       <div className="info-group">
+//         <img src="./images/mail-icon.png" />
+//         <p>{props.email}</p>
+//       </div>
+//     </div>
+//   );
+// }
