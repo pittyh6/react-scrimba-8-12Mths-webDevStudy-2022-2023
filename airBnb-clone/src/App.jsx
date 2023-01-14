33e62e3f7482edad13bdfd115de5a5c8ce58e0e2
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-01-15 10:42:19 +1100

    challenge props

--- airBnb-clone/src/App.jsx
+++ airBnb-clone/src/App.jsx
@@ -2,13 +2,35 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import zaferes from "../src/assets/zaferes.png";
+
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card/>
+      <Card
+        img= {zaferes}
+        rating="5.0"
+        reviewCount= {6}
+        country="USA"
+        titles="Life lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   );
 }
+
+
+/*
+Challenge: Pass props to the Card component and display that data
+
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
+*/
--- airBnb-clone/src/components/Card.jsx
+++ airBnb-clone/src/components/Card.jsx
@@ -1,41 +1,25 @@
 import React from "react";
 import star from "../assets/star.png";
 import zaferes from "../assets/zaferes.png";
-/*
-Challenge: Build the Card component
-For now, hard-code in the data (like 
-the rating, title, price, etc.)
+import App from "../App";
 
-Notes:
-- Only render 1 instance (I already did this for you)
-- The star icon and photo (katie-zaferes.png) are in the images 
-  folder for your use
-- Make sure to include:
-    - image
-    - star icon (star.png), rating, and review count
-    - title
-    - cost/person
-- The main purpose of this challenge is to show you where our limitations
-  currently are, so don't worry about the fact that you're hard-coding all
-  this data into the component.
-*/
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card container">
       <span className="card-status">SOLD OUT</span>
-      <img src={zaferes} alt="card big image" className="card-img" />
+      <img className="card-img" src= {props.img} />
       <div className="card-star-info">
         <img src={star} alt="card star image" className="card-img-star" />
-        <p className="card-star-number">5.0</p>
-        <p className="card-qtd-number">(6)</p>
+        <p className="card-star-number">{props.rating}</p>
+        <p className="card-qtd-number">{props.reviewCount}</p>
         <p className="card-location">
-          <span className="card-dot"></span> USA
+          <span className="card-dot"></span> {props.country}
         </p>
       </div>
-      <p className="card-title">Life lessons with Katie Zaferes</p>
+      <p className="card-title">{props.titles}</p>
       <p className="card-price">
-        <b>From $136 </b>  / person
+        <b>From ${props.price} </b>  / person
       </p>
     </div>
   );
