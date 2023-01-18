fa1e8ba5bdcb36a815609b0db23096d6d08787cc
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-01-18 20:46:28 +1100

    update/create data.js

---
+++ my-travel-jornal-solo-project/src/components/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        title: "Luna Park",
+        location: "Australia. Sydney",
+        googleMapsUrl: "https://goo.gl/maps/gLMFvPNnm7zTPeJW7",
+        startDate: "17 Set, 2018",
+        endDate: "08 Aug, 2022",
+        description: "Longstanding fun park reopened in 2004 after redevelopment, including a big top and a ferris wheel.",
+        imageUrl: "../public/images/LunaPark.jpg"
+    },
+    {
+        title: "Australian Reptile Park",
+        location: "Australia. Somersby.",
+        googleMapsUrl: "hhttps://goo.gl/maps/ZkFJzBDcm5EpoMbn6",
+        startDate: "17 Dec, 2020",
+        endDate: "08 Aug, 2022",
+        description: "Popular zoo with many species of reptiles, free-roaming kangaroos, shows & hands-on experiences.",
+        imageUrl: "../public/images/reptilePark.jpg"
+    },
+    {
+        title: "Opera House",
+        location: "Australia. Sydney",
+        googleMapsUrl: "https://goo.gl/maps/pBErjSQTD7GMKPNT7",
+        startDate: "17 April, 2017",
+        endDate: "08 Aug, 2022",
+        description: "Landmark, skyline-dominating arts centre for opera, theatre, music and dance, plus guided tours.",
+        imageUrl: "../public/images/Opera.jpg"
+    },
+    {
+        title: "Brighton-Le-Sands Beach",
+        location: "Australia. Brighton-Le-Sands.",
+        googleMapsUrl: "https://goo.gl/maps/xyvrPzsq3NULzDzJ7",
+        startDate: "25 Feb, 2010",
+        endDate: "08 Aug, 2022",
+        description: "Protected swimming area with a shaded promenade & views of planes landing at nearby Sydney Airport.",
+        imageUrl: "../public/images/reptilePark.jpg"
+    },
+]
