f4859e8c293b4e47cb022272352787903b46ed19
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-01-18 20:37:09 +1100

    my travel jhournal challenge header done

--- my-travel-jornal-solo-project/src/App.jsx
+++ my-travel-jornal-solo-project/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Header from './components/Header'
+import Main from './components/Main'
 
 function App() {
 
   return (
-    <h1>Hello</h1>
+    <div>
+      <Header/>
+      <Main/>
+    </div>
   )
 }
 
