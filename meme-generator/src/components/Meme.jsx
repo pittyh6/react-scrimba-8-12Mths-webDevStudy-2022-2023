bb171cebd042d09c3006e26c9fa110a28fed37a3
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-01-21 21:50:50 +1100

    add random img return on console.log

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import memesData from "../memesData";
 
 export default function Meme() {
-  console.log(memesData)
-  function memeImg(){
-    console.log(memesData.data.memes[Math.floor(Math.random()*99)].url)
+  function getMemeImg(){
+    console.log(memesData.data.memes[Math.floor(Math.random() * memesData.data.memes.length)].url)
   }
   return (
     <main className="meme-form">
       <div className="form">
         <input className="form-text form-top-text" type="text" placeholder=" Top Text" />
         <input className="form-text form-bottom-text" type="text" placeholder=" Bottom Text"/>
-        <button className="form-btn" onClick={memeImg}>Get a new meme image 🖼️</button>
+        <button className="form-btn" onClick={getMemeImg}>Get a new meme image 🖼️</button>
       </div>
     </main>
   );
