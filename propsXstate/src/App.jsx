971c4c97059b3966d5b2408bbbe0e8ee23b884e8
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-01-22 15:13:14 +1100

    challenge React.useState

--- propsXstate/src/App.jsx
+++ propsXstate/src/App.jsx
@@ -4,13 +4,13 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const result = React.useState("Yes")
-  console.log(result);
+  const [isImportant,func] = React.useState("Yes")
+  console.log(isImportant);
   return (
     <div className="state">
       <h1 className="state--title">Is state important to know?</h1>
       <div className="state--value">
-        <h1>{result[0]}</h1>
+        <h1>{isImportant}</h1>
       </div>
     </div>
   );
