79ba5a4a717abe795e5b811cc437942eed4c558f
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-01-23 08:39:34 +1100

    alter if else statement to ternary condition inside the tag html

--- propsXstate/src/Ternary.jsx
+++ propsXstate/src/Ternary.jsx
@@ -5,16 +5,15 @@ export default function Ternary(){
      * Challenge: Replace the if/else below with a ternary
      * to determine the text that should display on the page
      */
-     const isGoingOut = true
+     const isGoingOut = false
     
-     let answer = isGoingOut ? "Yes" : "No"
     
      
      return (
          <div className="state">
              <h1 className="state--title">Do I feel like going out tonight?</h1>
              <div className="state--value">
-                 <h1>{answer}</h1>
+                 <h1>{isGoingOut ? "Yes" : "No"}</h1>
              </div>
          </div>
      )
