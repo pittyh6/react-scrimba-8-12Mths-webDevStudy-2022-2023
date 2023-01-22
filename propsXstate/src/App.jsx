f5eda1e71ce722f15364d2fc7ffee82130bddf1c
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-01-22 15:34:55 +1100

    challenge React.useState click

--- propsXstate/src/App.jsx
+++ propsXstate/src/App.jsx
@@ -4,16 +4,29 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const [isImportant,func] = React.useState("Yes")
-  console.log(isImportant);
+  const [isImportant, setIsImportant] = React.useState("Yes")
+  /**
+   * Challenge: 
+   * 1. Create a function called `handleClick` that runs
+   *    setIsImportant("No")
+   * 2. add a click event listener to the div.state--value
+   *    that runs `handleClick` when the div is clicked.
+   */
+  
+  function handleClick() {
+      return (
+        setIsImportant("No")
+      )
+  }
+  
   return (
-    <div className="state">
-      <h1 className="state--title">Is state important to know?</h1>
-      <div className="state--value">
-        <h1>{isImportant}</h1>
+      <div className="state">
+          <h1 className="state--title">Is state important to know?</h1>
+          <div className="state--value" onClick={handleClick}>
+              <h1>{isImportant}</h1>
+          </div>
       </div>
-    </div>
-  );
+  )
 }
 
 export default App;
