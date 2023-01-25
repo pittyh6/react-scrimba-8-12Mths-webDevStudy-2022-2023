08dce71bb117fb5f067dc7abfcf2f17e95e07860
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-01-25 20:34:21 +1100

    obrject state challenge

--- meme-challenges-study/src/main.jsx
+++ meme-challenges-study/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import StateObject from './StateObject'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <StateObject />
   </React.StrictMode>,
 )
